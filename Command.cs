4e9183e3e4c9e8d953a0f5d9529fc75da0037be7
matt7188 <matt@thompson.org>
2020-06-24 20:11:59 -0400

    All Non assets

---
+++ Command.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Command
+{
+    public Name GoingTo;
+
+    public Items Lookingfor;
+
+    public People Finding;
+}
