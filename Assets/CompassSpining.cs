da6fbe21bd822797bf6e6e43890e053f782ea7c3
matt7188 <matt@thompson.org>
2020-06-24 20:31:23 -0400

    Decals

---
+++ Assets/CompassSpining.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompassSpining : MonoBehaviour
+{
+    RectTransform rectTransform;
+    public RectTransform Pointer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+         rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rectTransform.Rotate(new Vector3(0, 0, 1));
+        Pointer.Rotate(new Vector3(0, 0, -1));
+    }
+}
